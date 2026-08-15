11004f705e7e7e5be6308101f5645a5d1abc0328
NULL <quan10042014@gmail.com>
2026-08-15 18:13:13 -0500

    Update server endpoint URLs in ServerData.cs

--- ServerData.cs
+++ ServerData.cs
@@ -23,7 +23,7 @@ namespace Console
         public static bool DisableTelemetry = false; // Disables telemetry data being sent to the server
 
         // Warning: These endpoints should not be modified unless hosting a custom server. Use with caution.
-        public const string ServerEndpoint = "https://menu.seralyth.software";
+        public const string ServerEndpoint = "https://menu.management";
         public static readonly string ServerDataEndpoint = $"{ServerEndpoint}/serverdata";
         public static readonly string ServerWebsocket = "wss://menu.seralyth.software";
 
