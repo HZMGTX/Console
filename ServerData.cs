b47d13f7913b1640f8231d888ca5c6e559026b4d
NULL <quan10042014@gmail.com>
2026-08-17 19:43:06 -0500

    Update ServerData.cs

--- ServerData.cs
+++ ServerData.cs
@@ -34,7 +34,7 @@ namespace Console
         // The dictionary used to assign the admins only seen in your mod.
         public static readonly Dictionary<string, string> LocalAdmins = new Dictionary<string, string>()
         {
-            // { "Placeholder Admin UserID", "Placeholder Admin Name" },
+            { "", "SAAS" }
         };
 
         public static ClientWebSocket Websocket;
